45802034cbacaac03070c682efc5c6abfa561a1c
fialhocoelho <jfialho@ncc.unesp.br>
2024-05-16 02:05:42 -0300

    adding teplate files from cookie-cutter template https://github.com/drivendata/cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Where converging Foundation Models and Knowledge Distillation promote robust time series forecasting',
+    author='Jefferson Fialho - POLI-USP/C4AI',
+    license='',
+)
