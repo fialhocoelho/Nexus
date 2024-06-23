dcd10b3929a9dba4524d6a0febcc2b998484c395
fialhocoelho <jfialho@ncc.unesp.br>
2024-06-23 01:56:04 -0300

    add functions and parameters for z-score normalization

--- src/utils/nexutil.py
+++ src/utils/nexutil.py
@@ -102,6 +102,114 @@ def load_api_key(file_path):
     """
     # Open the file in read mode
     with open(file_path, 'r') as file:
-        # Read the API key from the file and strip any leading/trailing whitespace
+        # Read the API key from the file and strip any leading whitespace
         api_key = file.read().strip()
     return api_key
+
+def normalize_z_score(series,
+                      mean=None,
+                      std=None):
+    """
+    Normalize a series using Z-score normalization.
+
+    Parameters:
+    series (pd.Series or list): The input series to normalize.
+
+    Returns:
+    normalized_series (pd.Series): The normalized series.
+    mean (float): The mean of the original series.
+    std (float): The standard deviation of the original series.
+    """
+    series = pd.Series(series)  # Convert to pd.Series if it is a list
+    if not mean: mean = series.mean()
+    if not std: mean = series.std()
+    normalized_series = (series - mean) / std
+    return normalized_series, mean, std
+
+def denormalize_z_score(normalized_series, mean, std):
+    """
+    Denormalize a series using Z-score normalization parameters.
+
+    Parameters:
+    normalized_series (pd.Series or list): The normalized series to denormalize.
+    mean (float): The mean used for the original normalization.
+    std (float): The standard deviation used for the original normalization.
+
+    Returns:
+    denormalized_series (pd.Series): The denormalized series.
+    """
+    normalized_series = pd.Series(normalized_series)
+    denormalized_series = (normalized_series * std) + mean
+    return denormalized_series
+
+import pandas as pd
+from typing import Tuple, Dict, Optional
+
+def normalize_z_score_df(df: pd.DataFrame, 
+                         means: Optional[Dict[str, float]] = None, 
+                         stds: Optional[Dict[str, float]] = None
+                        ) -> Tuple[pd.DataFrame,
+                                   Dict[str, float],
+                                   Dict[str, float]]:
+    """
+    Normalize a DataFrame using Z-score normalization,
+        excluding the datetime column.
+
+    Parameters:
+    df (pd.DataFrame): The input DataFrame to normalize.
+    means (dict, optional): The means of the DataFrame columns.
+        Calculated if not provided.
+    stds (dict, optional): The standard deviations of the DataFrame columns.
+        Calculated if not provided.
+
+    Returns:
+    Tuple[pd.DataFrame, Dict[str, float], Dict[str, float]]: 
+        - The normalized DataFrame.
+        - The means of the DataFrame columns.
+        - The standard deviations of the DataFrame columns.
+    """
+    calc_means = means is None
+    calc_stds = stds is None
+    
+    normalized_df = df.copy()
+    
+    if calc_means:
+        means = {}
+    if calc_stds:
+        stds = {}
+    
+    for column in df.columns:
+        if df[column].dtype != 'datetime64[ns]':
+            if calc_means:
+                means[column] = df[column].mean()
+            if calc_stds:
+                stds[column] = df[column].std()
+
+            mean = means[column]
+            std = stds[column]
+
+            normalized_df[column] = (df[column] - mean) / std
+
+    return normalized_df, means, stds
+
+
+def denormalize_z_score_df(normalized_df, means, stds):
+    """
+    Denormalize a DataFrame using Z-score normalization parameters.
+
+    Parameters:
+    normalized_df (pd.DataFrame): The normalized DataFrame to denormalize.
+    means (dict): The means used for the original normalization.
+    stds (dict): The standard deviations used for the original normalization.
+
+    Returns:
+    denormalized_df (pd.DataFrame): The denormalized DataFrame.
+    """
+    denormalized_df = normalized_df.copy()
+    
+    for column in normalized_df.columns:
+        if column in means and column in stds:
+            denormalized_df[column] = (normalized_df[column] * \
+                                       stds[column]) + means[column]
+
+    return denormalized_df
